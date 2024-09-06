44f697c27bd2b3dd477546f6aa215755c1889d03
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2024-09-06 16:35:42 +0300

    added animations

--- src/app/link-list/link-list.component.ts
+++ src/app/link-list/link-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-link-list',
